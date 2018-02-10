8e149abb335a707f1e55797dce1132796b4ccb0b
Bart <bart@paybase.io>
2018-02-10 18:36:39 +0000

    saving todo in mongodv

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const mongoose = require('./db/mongoose');
-const User = require('./models/user');
-const Todo = require('./models/todo');
+const { mongoose } = require('./db/mongoose');
+const { User } = require('./models/user');
+const { Todo } = require('./models/todo');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,7 +10,12 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  console.log(req.body);
+  const todo = new Todo({
+    text: req.body.text,
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, e => res.status(400).send(e));
 });
 
 
